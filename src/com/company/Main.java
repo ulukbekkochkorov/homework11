74ffb9f9497039e4a7a618873f99fd4c6fece758
Ulukbek <ulukbekkochkorov@gmail.com>
2022-05-05 15:42:52 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Triangle triangle = new Triangle();
+        int a = 9;
+        int b = 8;
+        int c = 10;
+    int p = (a+b+c)/2;
+    int i = (p*(p-a)*(p-b)*(p-c));
+     double squer = Math.sqrt(i);
+        System.out.println(squer);
+    }
+}
---
+++ src/com/company/Triangle.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Triangle {
+    public int a;
+    public int b;
+    public int c;
+    public int p;
+    public int squer;
+}
